3d962facf24a0ab45f10efa8456e6b2022b6369c
Huang Hsing <newyellow@Huangteki-MacBook-Pro-3.local>
2012-10-29 15:02:11 +0800

    Add MapCameraControl Class file and a Map Picture

---
+++ LightBugBattle/MapCameraControl.h
@@ -0,0 +1,32 @@
+//
+//  MapCameraControl.h
+//  MapTest
+//
+//  Created by Huang Hsing on 12/10/24.
+//  Copyright 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "cocos2d.h"
+
+@interface MapCameraControl : CCNode
+{
+    CCSprite* map;
+    CCCamera* mainCamera;
+    CGPoint cameraPosition;
+    CGSize mapSize;
+    CGSize winSize;
+    float heightLimit;
+    float widthLimit;
+    
+    CCSprite* target;
+    BOOL isMove;
+    CGPoint moveAmount;
+}
+
+-(void) setMap:(CCSprite*)theMap mapLayer:(CCNode*)mapLayer;
+-(void) moveCameraX:(float)moveX Y:(float)moveY;
+-(void) moveCameraToX:(float)moveX Y:(float)moveY;
+-(void) followTarget:(CCSprite*)theTarget;
+
+@end
---
+++ LightBugBattle/MapCameraControl.m
@@ -0,0 +1,66 @@
+//
+//  MapCameraControl.m
+//  MapTest
+//
+//  Created by Huang Hsing on 12/10/24.
+//  Copyright 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import "MapCameraControl.h"
+
+
+@implementation MapCameraControl
+
+-(id) init
+{
+    if((self=[super init]))
+    {
+        target = NULL;
+        isMove = NO;
+        moveAmount = ccp(0,0);
+        cameraPosition = ccp(0,0);
+        winSize = [CCDirector sharedDirector].winSize;
+        [self moveCameraToX:0 Y:0];
+    }
+    
+    return self;
+}
+
+-(void) setMap:(CCSprite*)theMap mapLayer:(CCNode*)mapLayer
+{
+    map = theMap;
+    mainCamera = mapLayer.camera;
+    mapSize = theMap.boundingBox.size;
+    heightLimit = (mapSize.height - winSize.height)/2;
+    widthLimit = (mapSize.width - winSize.width)/2;
+    [map setPosition:ccp(0,0)];
+    [mapLayer setPosition:ccp(winSize.width/2, winSize.height/2)];
+}
+
+-(void) moveCameraX:(float)moveX Y:(float)moveY
+{
+    [self moveCameraToX:cameraPosition.x + moveX Y:cameraPosition.y + moveY];
+}
+ 
+-(void) followTarget:(CCSprite*)theTarget
+{
+    target = theTarget;
+    [self schedule:@selector(followUpdate:)];
+}
+
+-(void) moveCameraToX:(float)moveX Y:(float)moveY
+{
+    cameraPosition.y = MIN( heightLimit, MAX( -1*heightLimit, moveY ) );
+    cameraPosition.x = MIN( widthLimit, MAX( -1*widthLimit, moveX ) );
+    
+    [mainCamera setCenterX:cameraPosition.x centerY:cameraPosition.y centerZ:0];
+    [mainCamera setEyeX:cameraPosition.x eyeY:cameraPosition.y eyeZ:415];
+}
+
+-(void) followUpdate:(ccTime) dt
+{
+    [self moveCameraToX:target.position.x Y:target.position.y];
+}
+
+@end
+
