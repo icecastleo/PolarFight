8d8e5f6b423e9a7940e6b7ddf820c707c586ae71
陳 謙 <cloudsan@cloudsan.(none)>
2012-11-12 18:56:14 +0800

    update collision

--- LightBugBattle/Helper.m
+++ LightBugBattle/Helper.m
@@ -14,57 +14,43 @@
 //void calc(circle cir1,circle cir2)
 +(CGPoint) moveRedirectWhileCollisionP1:(CGPoint)point1 R1:(float)r1 P2:(CGPoint)point2 R2:(float)r2 Location:(CGPoint)location
 {
-    double x1,x2,y1,y2;//此為兩圓相交的坐標
-    if(point1.y!=point2.y)//兩圓圓心Y值不同時
-    {//m= y=mx+k的x項系數、k= y=mx+k的k項常數、 a、b、c= x=(-b±√(b^2-4ac))/2a的係數
-        double m=(point1.x-point2.x)/(point2.y-point1.y),k=(pow(r1,2)-pow(r2,2)+pow(point2.x,2)-pow(point1.x,2)+pow(point2.y,2)-pow(point1.y,2))/(2*(point2.y-point1.y));
-        double a=1+pow(m,2),b=2*(k*m-point2.x-m*point2.y),c=pow(point2.x,2)+pow(point2.y,2)+pow(k,2)-2*k*point2.y-pow(r2,2);
+    
+    if(ccpDistance(location, point2)>r1+r2)
+        return location;
+    CGPoint moveVector= ccpSub(location, point1);
+    CGPoint pointVector = ccpSub(point2, point1);
+    
+    float originalVectorAngle = [self calculateVectorAngle:moveVector.x y:moveVector.y];
+    float twoPointAngle = [self calculateVectorAngle:pointVector.x y:pointVector.y];
+    CGPoint resultVector = moveVector;
+    
+    while (true) {
         
-        if(b*b-4*a*c>=0)//有交點時
-        {
-            x1=((-b)+sqrt(b*b-4*a*c))/(2*a);//x=(-b+√(b^2-4ac))/2a
-            y1=m*x1+k;//y=mx+k
-            x2=((-b)-sqrt(b*b-4*a*c))/(2*a);//x=(-b-√(b^2-4ac))/2a
-            y2=m*x2+k;//y=mx+k
-            if(b*b-4*a*c>0)//兩交點
-            {
-                printf("The cross points are (%.2lf,%.2lf) and (%.2lf,%.2lf).\n",x1,y1,x2,y2);
-            
-            }
-            else//一交點
-            {
-                printf("The cross points are (%.2lf,%.2lf).\n",x1,y1);
-            }
-        }
-        else//沒有交點時
-        {
-            printf("No cross points.\n");
-        }
+        resultVector= [self RotatePointAboutOrigin:resultVector Angle:M_PI/36];
+        CGPoint newLocation =ccpAdd(point1, resultVector);
+        if(ccpDistance(newLocation, point2)>r1+r2)
+            return newLocation;
     }
-    else if(point1.y==point2.y)//兩圓圓心Y值相同時
-    {//x1= 兩交點的x值、 a、b、c= x=(-b±√(b^2-4ac))/2a的係數
-        x1=-(pow(point1.x,2)-pow(point2.x,2)-pow(r1,2)+pow(r2,2))/(2*point2.x-2*point1.x);
-        double a=1,b=-2*point1.y,c=pow(x1,2)+pow(point1.x,2)-2*point1.x*x1+pow(point1.y,2)-pow(r1,2);
-        if(b*b-4*a*c>=0)
-        {
-            y1=((-b)+sqrt(b*b-4*a*c))/(2*a);//y=(-b+√(b^2-4ac))/2a
-            y2=((-b)-sqrt(b*b-4*a*c))/(2*a);//y=(-b-√(b^2-4ac))/2a
-            if(b*b-4*a*c>0)//兩交點
-            {
-                if( ccpDistance(location, ccp(x1,y1)) > ccpDistance(location, ccp(x2,y2)) )
-                {
-                    
-                }
-            }
-            else//一交點
-            {
-                printf("The cross points are (%.2lf,%.2lf).\n",x1,y1);
-            }
-        }
-        else//沒有交點時
-        {
-            printf("No cross points.\n");
-        }
-    }
+    
+    return location;
 }
+
++(CGPoint) RotatePointAboutOrigin:(CGPoint) point Angle:(float)angle
+{
+    float s = sinf(angle);
+    float c = cosf(angle);
+    return CGPointMake(c * point.x - s * point.y, s * point.x + c * point.y);
+}
+
++(float) calculateVectorAngle:(float)x y:(float)y
+{
+    float angleRadians = atanf(x/y);
+    float angleDegrees = CC_RADIANS_TO_DEGREES(angleRadians);
+    float cocosAngle =-1* angleDegrees;
+    if (x<0) {
+        cocosAngle +=180;
+    }
+return cocosAngle;
+}
+
 @end
